1bbee3df6d4027b11caa5e613a76f39f143a9e7d
John Hitchings <john@wealthfront.com>
2017-06-21 11:15:06 -0700

    refactor for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pipeflow',
+  packages = ['pipeflow'],
+  version = '0.1',
+  description = 'A clone of Spotify\'s Luigi library with less features and intended for workflows on a single machine.',
+  author = 'John Hitchings',
+  author_email = 'john@wealthfront.com',
+  url = 'https://github.com/hitch17/pipeflow',
+  download_url = 'https://github.com/hitch17/pipeflow/archive/0.1.tar.gz',
+  keywords = ['python', 'workflow'],
+  classifiers = [],
+)
