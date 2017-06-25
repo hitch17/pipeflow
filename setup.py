a111b9762f800c10b984a44e3e7b4ecf3b6d8f26
John Hitchings <john@wealthfront.com>
2017-06-25 12:35:38 -0700

    bump to 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pipeflow',
   packages = ['pipeflow'],
-  version = '0.3',
+  version = '0.4',
   description = 'A clone of Spotify\'s Luigi library with less features and intended for workflows on a single machine.',
   author = 'John Hitchings',
   author_email = 'john@wealthfront.com',
   url = 'https://github.com/hitch17/pipeflow',
-  download_url = 'https://github.com/hitch17/pipeflow/archive/0.3.tar.gz',
+  download_url = 'https://github.com/hitch17/pipeflow/archive/0.4.tar.gz',
   keywords = ['python', 'workflow'],
   classifiers = [],
 )
